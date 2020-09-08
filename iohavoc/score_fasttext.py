8db904274a76205bc006cae45780d5d4e24023a7
Iroro Orife <iorife@netflix.com>
2020-09-08 15:53:13 -0700

    [ADD] WIP example for fasttext language id

---
+++ iohavoc/score_fasttext.py
@@ -0,0 +1,18 @@
+import sys
+import fasttext
+
+
+PRETRAINED_MODEL_PATH = '/tmp/lid.176.bin'
+
+if __name__ == '__main__':
+
+    if(len(sys.argv) < 2):
+        print("Usage: python score_fasttext.py [a-sentence]\n")
+        exit(1)
+
+    model = fasttext.load_model(PRETRAINED_MODEL_PATH)
+    sentence = sys.argv[1]
+    
+    sentences = [sentence]
+    predictions = model.predict(sentences)
+    print(predictions)
